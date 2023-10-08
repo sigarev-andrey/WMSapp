d3371afc1e68b9cd67d2015f08cabeac628ad6ab
Sigarev Andrey <sigarev.andrey@yandex.ru>
2023-10-08 13:29:44 +0300

    Add admin filters
    -add item admin filters
    -add storage admin filter
    -add manufacturer admin filter

--- storage/admin.py
+++ storage/admin.py
@@ -7,10 +7,37 @@ from .models import Storage
 from .models import Contract
 from .models import ItemInRelease
 
-admin.site.register(Manufacturer)
 admin.site.register(Category)
 admin.site.register(Unit)
-admin.site.register(Item)
-admin.site.register(Storage)
 admin.site.register(Contract)
 admin.site.register(ItemInRelease)
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'category')
+    search_fields = ['manufacturer__name', 'article', 'description']
+    list_filter = ['manufacturer', 'category']
+    list_per_page = 50
+
+admin.site.register(Item, ItemAdmin)
+
+class StorageAdmin(admin.ModelAdmin):
+    list_display = ('item', 'get_unit', 'count', 'get_category')
+    search_fields = ['item__manufacturer__name', 'item__article', 'item__description']
+    list_filter = ['item__manufacturer', 'item__category']
+    list_per_page = 50
+
+    @admin.display(ordering='item__unit', description='Unit')
+    def get_unit(self, obj):
+        return obj.item.unit
+    
+    @admin.display(ordering='item__category', description='Category')
+    def get_category(self, obj):
+        return obj.item.category
+
+admin.site.register(Storage, StorageAdmin)
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_per_page = 50
+
+admin.site.register(Manufacturer, ManufacturerAdmin)
